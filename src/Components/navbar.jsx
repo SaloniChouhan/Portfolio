00a251dfd5fe3abcf4073a1886e72d08817e8dea
Chouhan <saloni.chouhan@capgemini.com>
2023-10-27 15:44:42 +0530

    landing page positioning correction

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -7,7 +7,7 @@ import Resume from "../assets/Saloni_Chouhan_CV.pdf";
 const NavBar = () => {
   return (
     <>
-      <nav className="container flex pt-6 px-6 top-0 mt-2 sm:px-6 md:px-10 mx-auto items-center relative bg-darkest-green lg:max-w-7xl">
+      <nav className="container flex pt-6 px-6 top-0 mt-2 sm:px-6 md:px-10 mx-auto items-center absolute bg-darkest-green lg:max-w-7xl">
         <div className=" flex mx-auto items-center justify-between  w-full  ">
           <h1 className="font-serif text-base md:text-lg lg:text-xl font-medium  text-core-green-300">
             <span className="float-left">
